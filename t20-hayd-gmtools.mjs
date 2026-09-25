3068cbedaa050ab9b74a45963fb22b5ae9cc4ca4
ahahayd <ahahayd@users.noreply.github.com>
2026-09-25 03:09:34 -0300

    fix: configurações com nomes inteiros e Gerenciar Grupos na seção certa
    A etiqueta de impacto ficava dentro do rótulo, que o Foundry desenha com a
    altura do campo (32px) como altura de linha: quase todo nome quebrava ao
    meio com um vão grande entre as linhas. A etiqueta vai para a linha de
    baixo e o rótulo usa altura de linha normal.
    
    O botão Gerenciar Grupos não estava em nenhuma categoria e aparecia solto
    no topo; agora abre a seção Ficha do Grupo.

--- t20-hayd-gmtools.mjs
+++ t20-hayd-gmtools.mjs
@@ -1394,7 +1394,7 @@ const CATEGORIAS_CONFIG = [
   { rotulo: 'CatRolagens',    icone: 'fa-dice-d20',       chaves: ['jogadoresReroll', 'jogadoresManual'] },
   { rotulo: 'CatAutomacoes',  icone: 'fa-wand-sparkles',  chaves: ['automacoesEnabled', 'abrirDiarioAutomacoes'] },
   { rotulo: 'CatTesouros',    icone: 'fa-sack-dollar',    chaves: ['tesourosVinculosMenu', 'tesourosLivrosMenu', 'tesourosHomebrewMenu'] },
-  { rotulo: 'CatParty',       icone: 'fa-users',          chaves: ['partySheetEnabled', 'visibility', 'requireConfirmation', 'chatMode', 'lojaCompat'] },
+  { rotulo: 'CatParty',       icone: 'fa-users',          chaves: ['partyManager', 'partySheetEnabled', 'visibility', 'requireConfirmation', 'chatMode', 'lojaCompat'] },
   { rotulo: 'CatAtributos',   icone: 'fa-dice-d6',        chaves: ['atributosMetodoPadrao', 'atributosPontos', 'atributosMultiNegativos', 'atributosCustosMenu', 'atributosConversaoMenu'] },
   { rotulo: 'CatFerramentas', icone: 'fa-ruler-combined', chaves: ['reguaEfeitos', 'mensagensDano', 'previewDano'] },
   { rotulo: 'CatCorrecoes',   icone: 'fa-screwdriver-wrench', chaves: ['custoPmTotal'] }
